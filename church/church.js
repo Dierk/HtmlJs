bcfb7cc85a30e6f98d4fcc71763f1284cc9e8f6b
Dierk Koenig <dierk.koenig@canoo.com>
2018-01-15 12:11:15 +0100

    proofing the SK combinators from lambda calculus

--- church/church.js
+++ church/church.js
@@ -13,6 +13,9 @@ const beta = f => x => f(x);
 
 // self-application, Mockingbird, \x.x x
 const M = f => beta(f)(f);  // f(f)
+// M is SII
+// S(id)(id) (x) = id(x) (id(x))
+// S(id)(id) = M
 
 // Kestrel, \x. \y. x
 // M, const, first, id2, true
@@ -20,6 +23,11 @@ const konst = x => y => x;
 
 // Cardinal, \fxy.fyx
 const flip = f => x => y => f(y)(x);
+// const flip = f => g => x => f(x)(g);  // f(x)(g(x)) // konst(g)(x) -> g
+// const flip = f => g      => s(f)(konst(g));         // C = \fg.S(f)(Kg)
+// const flip = f => g => x => s(f)(konst(g))(x);      // def of S
+// const flip = f => g => x => f(x)(konst(g)(x));
+// const flip = f => g => x => f(x)(g); // qed.
 
 // Kite
 // kite = x => y => y;
@@ -32,22 +40,44 @@ const kite = flip(konst);
 
 // Bluebird, composition
 const cmp = f => g => x => f(g(x));
+// const cmp = f => g      => S(konst(f))(g); // B = \fg.S(Kf)g
+// const cmp = f => g => x => S(konst(f))(g)(x);
+// const cmp = f => g => x => (konst(f)(x))(g(x));
+// const cmp = f => g => x => (f)(g(x));
+// const cmp = f => g => x => f(g(x)); // qed.
 
 // Blackbird
 //const cmp2 = f => g => x => y => f(g(x)(y));
 const cmp2 = cmp (cmp)(cmp);
 
-// is there any such combinator?
-// const s = f => x => f(x)(x);
+// Starling, \abc.ac(bc)
+const S = f => g => x => f(x)(g(x));
+
+// identity is SKK, S(konst)(konst)
+// S(konst)(konst)(x) = konst(x)( konst(x) )
+// S(konst)(konst)(x) =      (x)
+// S(konst)(konst)(x) =    id(x)
+// S(konst)(konst)    =    id          // qed
+
 
 // ---- boolean logic
 
-const T = konst;
-const F = kite;
-const and = x => y => x(y)(x);
-const or  = M;
+const T   = konst;
 const not = flip;
+const F   = not(T);             //const F = kite;
+
+const and = x => y => x(y)(x);
+// const and = f => g => f(g)(f);
+// const and = f => g => S(f)(konst(f))(g)  // \fg.Sf(Kf)g
+
+const or  = x => y => x(x)(y);
+// const or  = x =>  x(x);
+// const or  = M;
+
 const beq = x => y => x(y)(not(y));
+//const beq = x => y => S(x)(not)(y);
+//const beq = x => S(x)(not);
+
 
 //const xor = cmp (cmp(not)) (beq)   ;
 const xor =  cmp2 (not) (beq)   ;
