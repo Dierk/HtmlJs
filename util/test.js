cb654f6f1d7ff02d6d18536d6c595b75567ec008
Dierk Koenig <dierk.koenig@canoo.com>
2018-03-03 17:40:47 +0100

    beautify test report code

--- util/test.js
+++ util/test.js
@@ -6,17 +6,21 @@ function report(origin, ok) {
     const extend = 20;
     if ( ok.every( elem => elem) ) {
         document.writeln(" "+ padLeft(ok.length, 3) +" tests in " + padRight(origin, extend) + " ok.");
-    } else {
-        document.writeln("+------------------------------------------+");
-        document.writeln("|    Failing tests in " + padRight(origin, extend) + " |");
-        for (let i = 0; i < ok.length; i++) {
-            if( ! ok[i]) {
-                document.writeln("|    Test #"+ padLeft(i, 3) +" failed                      |");
-            }
-        }
-        document.writeln("+------------------------------------------+");
+        return;
     }
+    let reportLine = "    Failing tests in " + padRight(origin, extend);
+    bar(reportLine.length);
+    document.writeln("|" + reportLine+ "|");
+    for (let i = 0; i < ok.length; i++) {
+        if( ! ok[i]) {
+            document.writeln("|    Test #"+ padLeft(i, 3) +" failed                     |");
+        }
+    }
+    bar(reportLine.length);
+}
 
+function bar(extend) {
+    document.writeln("+" + "-".repeat(extend) + "+");
 }
 
 // padRight :: String, Int -> String
