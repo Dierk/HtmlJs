6e119b6674728d048cfd8762cea5cdad6ab0d088
Dierk Koenig <dierk.koenig@canoo.com>
2018-03-09 15:25:45 +0100

    plotter code beautification with currying

--- plotter/plotter.js
+++ plotter/plotter.js
@@ -6,8 +6,8 @@ const maxY =  1;
 
 function start() {
     const userFunction = document.getElementById('user_function');
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext("2d");
+    const canvas       = document.getElementById('canvas');
+    const context      = canvas.getContext("2d");
     display(context, x => eval( userFunction.value));
 
     userFunction.onchange = evt => display(context, x => eval( userFunction.value));
@@ -18,23 +18,26 @@ function display(context, f) {
     context.fillStyle = "papayawhip";
     context.fillRect(0, 0, canvas.width, canvas.height);
     // draw the function plot
+    normx = normalizeX(canvas.width);
+    normy = normalizeY(canvas.height);
+
     context.fillStyle = "black";
     context.beginPath();
-    context.moveTo(normalizeX(minX,canvas.width), normalizeY(f(minX),canvas.height));
+    context.moveTo(normx(minX), normy(f(minX)));
 
-    const stride =  (maxX - minX) / 100; // 100 Stützstellen
-    for (let x=minX; x<=maxX; x+=stride) {
-        context.lineTo(normalizeX(x,canvas.width), normalizeY(f(x),canvas.height));
+    const stride = (maxX - minX) / 100; // 100 Stützstellen
+    for (let x = minX; x <= maxX; x += stride) {
+        context.lineTo(normx(x), normy(f(x)));
         context.stroke();
     }
 }
 
-function normalizeY(y, height){
+const normalizeY = height => y => {
     let scaleFactor = height / (maxY - minY);
     return height - (y - minY) * scaleFactor;
-}
+};
 
-function normalizeX(x, width){
+const normalizeX = width => x => {
     let scaleFactor = width / (maxX - minX);
     return ( x - minX) * scaleFactor;
-}
+};
