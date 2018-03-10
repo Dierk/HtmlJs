40c3ded4f30b0374ffdcf242648fd0b16b3e2642
Dierk Koenig <dierk.koenig@canoo.com>
2018-03-10 14:43:38 +0100

    some more tests on observable Attributes wrt isolation and "new"

--- observable/observableTest.js
+++ observable/observableTest.js
@@ -16,5 +16,27 @@
 //  value is updated
     ok.push(attribute.getValue() === "firstValue");
 
+//  it still works when the receiver symbols changes
+    const newRef = attribute;
+    newRef.setValue("secondValue");
+    // listener updates correctly
+    ok.push(found === "secondValue");
+
+//  Attributes are isolated, no "new" needed
+    const secondAttribute = Attribute();
+
+//  initial state
+    ok.push(secondAttribute.getValue() === "");
+
+//  subscribers get notified
+    let secondFound;
+    secondAttribute.subscribe(val => secondFound = val);
+    secondAttribute.setValue("thirdValue");
+    ok.push(found === "secondValue");
+    ok.push(secondFound === "thirdValue");
+
+//  value is updated
+    ok.push(secondAttribute.getValue() === "thirdValue");
+
     report("observable", ok);
 }) ();
--- plotter/plotterTest.js
+++ plotter/plotterTest.js
@@ -4,11 +4,11 @@
 ( () => {
     let ok = [];
 
-    ok.push( normalizeY(-1, 100) === 100 ); // baseline
-    ok.push( normalizeY( 1, 100) === 0 );   // scale to top
+    ok.push( normalizeY(100)(-1) === 100 ); // baseline
+    ok.push( normalizeY(100)( 1) === 0 );   // scale to top
 
-    ok.push( normalizeX( 0, 100) === 0 );   // left origin
-    ok.push( normalizeX( 6, 100) === 100 ); // scale to right end
+    ok.push( normalizeX(100)( 0) === 0 );   // left origin
+    ok.push( normalizeX(100)( 6) === 100 ); // scale to right end
 
     report("plotter", ok);
 }) ();
