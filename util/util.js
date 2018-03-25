23ce7d851bc85eceaf01ef093a3cc0f3285e2322
Dierk Koenig <dierk.koenig@canoo.com>
2018-03-25 23:04:01 +0200

    put times() in util and test it

--- util/util.js
+++ util/util.js
@@ -1,9 +1,6 @@
 // https://stackoverflow.com/questions/10993824/do-something-n-times-declarative-syntax
 
 const timesFunction = function(callback) {
-  if (typeof callback !== "function" ) {
-    throw new TypeError("Callback is not a function");
-  }
   if( isNaN(parseInt(Number(this.valueOf()))) ) {
     throw new TypeError("Object is not a valid number");
   }
