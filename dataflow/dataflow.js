a6a520624f7ccd9da48434c2f9a13d6497329873
Dierk Koenig <dierk.koenig@canoo.com>
2018-04-05 13:09:54 +0200

    add Scheduler for calling async tasks in strict sequence

--- dataflow/dataflow.js
+++ dataflow/dataflow.js
@@ -9,3 +9,28 @@ const DataFlowVariable = howto => {
         return value;
     }
 };
+
+// execute asynchronous tasks in strict sequence
+const Scheduler = () => {
+    let inProcess = false;
+    const tasks = [];
+    function process() {
+        if (inProcess) return;
+        if (tasks.length === 0) return;
+        inProcess = true;
+        const task = tasks.pop();
+        const prom = new Promise( (ok, reject) => task(ok) );
+        prom.then( _ => {
+            inProcess = false;
+            process();
+        });
+    }
+    function add(task) {
+        tasks.unshift(task);
+        process();
+    }
+    return {
+        add: add,
+        addOk: task => add( ok => { task(); ok(); }) // convenience
+    }
+};
