167512e3b2de4154901d06a539d364629d18c15f
Dierk Koenig <dierk.koenig@canoo.com>
2018-04-24 16:59:02 +0200

    html generation function utility/ along the lines of elm/ps/react

--- util/util.js
+++ util/util.js
@@ -11,3 +11,29 @@ const timesFunction = function(callback) {
 
 String.prototype.times = timesFunction;
 Number.prototype.times = timesFunction;
+
+const nums = max => Array.from({length: max}, (v, i) => i);
+
+
+const html = (elementName, arg1, arg2) => parent => {
+  const root = document.createElement(elementName);
+  let childSpecs = (arg1 instanceof Array) ? arg1 : arg2;
+  if (arg1 !== undefined && arg1 !== null && childSpecs !== arg1) { // props are in args1
+      Object.keys(arg1).forEach( (key,index) => {
+          root.setAttribute(key, arg1[key]);
+      });
+  }
+  if (childSpecs !== undefined && childSpecs !== null) { // could still be missing
+    childSpecs.forEach( child => {
+      if (typeof(child) === 'string') {
+        root.innerText += child;
+      } else {
+        child(root);
+      }
+    });
+  }
+  if (parent !== undefined && parent !== null) {
+    parent.appendChild(root)
+  }
+  return root;
+};
--- util/utilTest.js
+++ util/utilTest.js
@@ -26,3 +26,31 @@ util.test("times-str", assert => {
     assert.is(collect[9], 9);
 
 });
+
+util.test("nums", assert => {
+   assert.is(nums(10).length, 10);
+   assert.is(nums(10)[0], 0);
+   assert.is(nums(10)[9], 9);
+});
+
+util.test("html", assert => {
+
+    const root = html('div',{class: 'del', innerText: 'X'}, [  // props and children
+        html('p'),                                             // neither
+        html('p',  {class: 'with more'}),                      // props
+        html('ul', {class: 'some'},
+            nums(10).map( it => html('li', [ 'text '+ it]))    // dynamic creation of children
+             .concat([html('li', [ 'some plain text' ])])      // no props, children
+        ),
+    ]);
+
+    const testroot = root(); // lazy init
+    assert.true(testroot instanceof HTMLElement);
+    assert.is(testroot.getAttribute('innerText'), 'X');
+    assert.is(testroot.childElementCount, 3);
+    // root(document.getElementsByTagName('body')[0]);  // changes the result view, only for visual testing
+    // assert.true(document.querySelector('.with') !== null);
+    assert.is(testroot.lastChild.lastChild.innerText, 'some plain text');
+
+
+});
