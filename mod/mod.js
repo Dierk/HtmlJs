0348fc52adcb9f9b898803aae8d150bcc57c3ef3
Dierk Koenig <dierk.koenig@canoo.com>
2018-11-10 22:50:58 +0100

    use js module system for safe singletons

--- mod/mod.js
+++ mod/mod.js
@@ -2,6 +2,16 @@
 
 // put import and exports on the top
 
-export { pi }
+export { pi, a, b, setA, setB }
 
 const pi = Math.PI;
+
+// use module as a singleton
+
+// make a single state that is only exposed as values, not references to objects
+
+let a = null; // these variables are exported as read-only
+let b = null;
+
+const setA = v => a = v;
+const setB = v => b = v;
--- mod/modTest.js
+++ mod/modTest.js
@@ -1,11 +1,31 @@
 
-import { pi } from './mod.js'
+import { pi, a, b, setA, setB } from './mod.js'
 
 export const suite = Suite('mod');
 
 suite.add("const", assert => {
 
     assert.true( pi > 0 ) ;
+
+});
+
+suite.add("singleton", assert => {
+
+    assert.is(a, null);
+    assert.is(b, null);
+
+    setA("Dierk"); // there is no object exposed and so no target to attack
+    setB("König");
+
+    assert.is(a, "Dierk");
+    assert.is(b, "König");
+
+    try {
+        a = "shall not work";
+        assert.true(false);
+    } catch (e) {
+        assert.true("exported variables are read-only.")
+    }
 
 });
 
