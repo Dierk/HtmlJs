4e0a8b228bd73f9d4508c0e3b205183db579602d
Dierk Koenig <dierk.koenig@canoo.com>
2020-03-20 20:50:21 +0100

    corona ball with buttons

--- ball/ball.js
+++ ball/ball.js
@@ -1,27 +1,28 @@
+const addButton         = document.getElementById("person");
+const infectedButton    = document.getElementById("infected");
+const canvas            = document.getElementById("canvas");
+const context           = canvas.getContext("2d");
+const radius            = 10;
+const max               = canvas.width;
+const balls             = [];
 
-const canvas  = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-context.fillStyle = "black";
-
-const radius = 10;
-const max = canvas.width;
 const rand = extend => Math.random() * extend;
 
-const balls = [];
-
-canvas.onclick = evt => {
+const add = infected => evt => {
     balls.push(
-        {x: evt.offsetX, y: evt.offsetY, dx:rand(6)-3, dy: rand(6)-3, infected: evt.shiftKey}
+        {x: max / 2, y: max / 2, dx:rand(8)-4, dy: rand(8)-4, infected: infected}
     )
 };
+addButton.onclick      = add(false);
+infectedButton.onclick = add(true);
 
 function start() {
     setInterval(() => {
         nextBoard();
-    }, 1000 / 50);
+    }, 1000 / 30);
 }
 
-const radiusSqr = radius**2
+const radiusSqr = radius**2;
 const touching = a => b => (a.x - b.x)**2 + (a.y - b.y)**2 < radiusSqr;
 
 function nextBoard() {
@@ -40,14 +41,14 @@ function nextBoard() {
 
 function display(ball) {
     context.fillStyle = "gold";
-    fillBox(ball, radius+1);
+    fillCircle(ball, radius + 1);
     ball.x = (ball.x + ball.dx + max) % max ;
     ball.y = (ball.y + ball.dy + max) % max ;
     context.fillStyle = ball.infected ? "red" : "black";
-    fillBox(ball, radius);
+    fillCircle(ball, radius);
 }
 
-function fillBox(ball, radius) {
+function fillCircle(ball, radius) {
     context.beginPath();
     context.arc(ball.x, ball.y, radius, 0, 6.3, false);
     context.fill();
