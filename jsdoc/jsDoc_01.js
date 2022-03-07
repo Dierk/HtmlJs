fac0ef39d7698d888fc1ea0ce26f45b08a423707
Dierk Koenig <dierk.koenig@canoo.com>
2022-03-07 14:59:59 +0100

    playing with jsdoc for educational purposes

---
+++ jsdoc/jsDoc_01.js
@@ -0,0 +1,20 @@
+/**
+ * @type { String }
+ */
+const myString = "hi";
+
+// const myString = 0;
+
+/**
+ * Logs a {@link String} Message.
+ * @function
+ * @param { String } message
+ * @return void
+ */
+const logMessage = message => {
+    console.log(message);
+}
+
+logMessage(myString);
+
+// logMessage(0);
---
+++ jsdoc/jsDoc_02.js
@@ -0,0 +1,8 @@
+/**
+ * @type { Array<String> }
+ */
+const myStrings = ["hi"];
+
+myStrings.push("CAS");
+
+myStrings.push(0);
