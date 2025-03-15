f4421ff015499faba1429c8019e1c4afe164fc9c
Dierk Koenig <dierk.koenig@canoo.com>
2025-03-15 17:24:17 +0100

    tetris 3D version - shapeO

--- box/scene/tetris/model.js
+++ box/scene/tetris/model.js
@@ -2,20 +2,35 @@
  * @module tetris/model
  */
 
-export { shapeITetros, shapeTTetros }
+export { shapeITetros, shapeTTetros, shape0Tetros }
+
+const shape0Tetros = [
+    [
+        {x: 0, y: 0, z: 0},
+        {x: 0, y: 1, z: 0},
+        {x: 0, y: 0, z: 1},
+        {x: 0, y: 1, z: 1},
+    ],
+    [
+        {x: 0, y: 0, z: 0},
+        {x: 1, y: 0, z: 0},
+        {x: 0, y: 0, z: 1},
+        {x: 1, y: 0, z: 1},
+    ],
+];
 
 const shapeITetros = [
     [
-        {x: 0, y: 1, z: 1},
-        {x: 1, y: 1, z: 1},
-        {x: 2, y: 1, z: 1},
-        {x: 3, y: 1, z: 1},
+        {x: 0, y: 1, z: 0},
+        {x: 1, y: 1, z: 0},
+        {x: 2, y: 1, z: 0},
+        {x: 3, y: 1, z: 0},
     ],
     [
-        {x: 1, y: 0, z: 1},
-        {x: 1, y: 1, z: 1},
-        {x: 1, y: 2, z: 1},
-        {x: 1, y: 3, z: 1},
+        {x: 1, y: 0, z: 0},
+        {x: 1, y: 1, z: 0},
+        {x: 1, y: 2, z: 0},
+        {x: 1, y: 3, z: 0},
     ],
 ];
 
