16d11d7bb50d306f4a120017ce6d242b6feb8b52
Dierk Koenig <dierk.koenig@canoo.com>
2025-03-16 22:44:28 +0100

    tetris 3D version - full roll and pitch

--- box/scene/tetris/controller.js
+++ box/scene/tetris/controller.js
@@ -2,14 +2,23 @@
  * @module controller
  */
 
-export { normalize, swapXZ, swapYZ };
+export { normalize, xzSwaps, yzSwaps };
 
 const normalize = tetro => {
     const minZ = Math.min(...tetro.map(box => box.z));
-    // const minY = Math.min(...tetro.map(box => box.y)); // commented since it might feel better to not normalize y
-    // return tetro.map( box => ({x: box.x, y: box.y - minY, z: box.z - minZ}));
     return tetro.map( box => ({x: box.x, y: box.y, z: box.z - minZ}));
 };
 
-const swapXZ = tetro => tetro.map( box => ({x: box.z, y: box.y, z: box.x}));
-const swapYZ = tetro => tetro.map( box => ({x: box.x, y: box.z, z: box.y}));
+const swapXZ        = tetro => tetro.map( box => ({x:  box.z, y: box.y, z:  box.x}));
+const negateX       = tetro => tetro.map( box => ({x: -box.x, y: box.y, z:  box.z}));
+const swapNegateXZ  = tetro => tetro.map( box => ({x: -box.z, y: box.y, z: -box.x}));
+
+const id = x => x;
+const xzSwaps = [ id, swapXZ, negateX, swapNegateXZ];
+
+
+const swapYZ        = tetro => tetro.map( box => ({x: box.x, y:  box.z, z:  box.y}));
+const negateY       = tetro => tetro.map( box => ({x: box.x, y: -box.y, z:  box.z}));
+const swapNegateYZ  = tetro => tetro.map( box => ({x: box.x, y: -box.z, z: -box.y}));
+
+const yzSwaps = [id, swapYZ, negateY, swapNegateYZ];
