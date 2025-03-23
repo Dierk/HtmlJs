874216e2b7a92802e7aa94b76de27a3fb1607bb3
Dierk Koenig <dierk.koenig@canoo.com>
2025-03-23 11:10:48 +0100

    server for sharing live updates

--- server/SharedTextServer.js
+++ server/SharedTextServer.js
@@ -2,6 +2,7 @@
 import { createServer }      from 'node:http';
 import { handleFileRequest } from "./fileRequestHandler.js";
 
+// small inline Observable that will most likely suffer from memory leaks
 const Observable = value => {
     const listeners = [];
     return {
@@ -16,43 +17,51 @@ const Observable = value => {
 };
 
 const hostname  = '127.0.0.1';
-const port      = 3000;
+const port      = 8080;
 const baseURL   = `http://${hostname}:${port}`
 
 const textObservable = Observable("");
 
 let eventId = 1;
 
-const server = createServer( (req, res) => {
-  console.dir(req.method, req.url);
-  if ( req.url === "/sharedText") {
+function handleSSEstart(res, req) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/event-stream');
     const lastEventId = req.headers['last-event-id'];
     if (lastEventId) {
-      console.info("got a last event id: " + lastEventId);
+        console.info("got a last event id: " + lastEventId);
     }
     const sendText = text => {
-      eventId++;
-      res.write('id:'     + eventId   + '\n');
-      res.write('event:'  + "sharedText" +'\n');
-      res.write('data:'   + JSON.stringify({text}) +'\n\n');
-    }
-    textObservable.onChange( sendText );
+        eventId++;
+        res.write('id:'    + eventId + '\n');
+        res.write('event:' + "sharedText" + '\n');
+        res.write('data:'  + JSON.stringify({text}) + '\n\n');
+    };
+    textObservable.onChange(sendText);
     sendText(textObservable.getValue());
-    return;
-  }
-  if ( req.url.startsWith("/updateText?") ) {
+}
+
+function handleTextUpdate(res, req) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    const text = new URL(baseURL+req.url).searchParams.get('text');
+    const text = new URL(baseURL + req.url).searchParams.get('text');
     textObservable.setValue(text);
     res.end("ok");
-    return;
+}
+
+const server = createServer( (req, res) => {
+  console.dir(req.method, req.url);
+  if ( req.url === "/sharedText") {
+      handleSSEstart(res, req);
+      return;
+  }
+  if ( req.url.startsWith("/updateText?") ) {
+      handleTextUpdate(res, req);
+      return;
   }
   handleFileRequest(req, res);
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at ${baseURL}`);
 });
